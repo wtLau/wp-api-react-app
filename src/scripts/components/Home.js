f4d8890b6e45ebc7894ae813a1adeeb9956c5623
Brian Lau <lauwangtatbrian@gmail.com>
2017-11-28 12:37:43 -0800

    add store to manipulate data

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DataStore from '../flux/actions/DataActions.js';
 
 export default class Home extends Component {
     render() {
+        let allData = DataStore;
+        console.log(allData);
+
         return (
             <div>
                 <h2>Hello World!</h2>
